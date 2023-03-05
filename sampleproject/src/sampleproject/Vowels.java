1d996e0a97db1933a99ffeb07287aeb5a1a4e8be
pratikkakad <pratikkakad216@gmail.com>
2023-03-05 18:30:12 +0530

    hii

--- sampleproject/src/sampleproject/Vowels.java
+++ sampleproject/src/sampleproject/Vowels.java
@@ -3,8 +3,19 @@ package sampleproject;
 public class Vowels {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		String s="mayuri jadahv prabhakar";
+		System.out.println(s.length());
+		int count =0;
+		for(int i=0; i<s.length();i++) {
+		char s1 = s.charAt(i);
+		if(s1=='a'|| s1=='e' || s1=='i' || s1=='o' || s1=='u') {
+			count++;
+			System.out.println(s.charAt(i));
+			}
+		}
+		
+		System.out.println("the given string has "+ count +" vowels");
 	}
 
 }
