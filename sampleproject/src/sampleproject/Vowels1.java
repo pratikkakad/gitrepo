b7c7084f5464a11828544d59c411ff8d620b11f7
pratikkakad <pratikkakad216@gmail.com>
2023-03-23 14:53:42 +0530

    vowels program

---
+++ sampleproject/src/sampleproject/Vowels1.java
@@ -0,0 +1,24 @@
+package sampleproject;
+
+public class Vowels1 {
+
+	public static void main(String[] args) {
+		
+	
+			
+			String s="mayuri jadahv prabhakar";
+			System.out.println(s.length());
+			int count =0;
+			for(int i=0; i<s.length();i++) {
+			char s1 = s.charAt(i);
+			if(s1=='a'|| s1=='e' || s1=='i' || s1=='o' || s1=='u') {
+				count++;
+				System.out.println(s.charAt(i));
+				}
+			}
+			
+			System.out.println("the given string has "+ count +" vowels");
+
+	}
+
+}
